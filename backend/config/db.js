a9e3ba8d196ab949e48fe42bf5d46d0752cb8487
Chetan Khajuria <chetankhajuria1493@gmail.com>
2024-10-29 18:16:18 +0530

    update

---
+++ backend/config/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+const MONGODB_URL = process.env.MONGODB_URL;
+export const connectDB = async () => {
+    await mongoose.connect(MONGODB_URL).then(()=>console.log("DataBase Connected"));
+}
